f5f9feaeb8b4eec40faab8af32ec2d772c20a2a3
EvilNeighbor <denissycheuski@gmail.com>
2022-04-11 11:07:10 +0300

    Firts commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+"use strict";
+// let number = 4.6;
+// const persone = "Alex";
+// const bool = true;
+// let und;
+
+// const obj = {
+//   name: "John",
+//   age: 25,
+//   isMarried: false,
+// };
+
+// let arr = ["plum.png", "orange.jpg", 6, 25, "apple.jpg"];
+
+// const rusult = confirm("Ate you 18 years old ");
+// const answer = prompt("How old are you?", "18");
+// console.log(answer * 5);
+
+// const answers = [];
+
+// answers[0] = prompt("What is your name?", "");
+// answers[1] = prompt("Haw old are toy?", "");
+// answers[2] = prompt("Are you married?", "");
+// console.log(typeof answers);
+
+// const category = "toys";
+// const user = "Ivan";
+// console.log(`https://google.com/${category}/5`);
+// alert(`Привет,${user}`);
+
+// console.log(4 + "5");
+
+// let incr = 20,
+//   decr = 15;/
+
+// ++incr;
+// --decr;
+// console.log(incr % decr);
+console.log(2 * 2 + 2 == "6");
+
+const a = false,
+  b = false;
+
+console.log(a || !b);
