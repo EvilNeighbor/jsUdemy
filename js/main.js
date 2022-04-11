9ecd31bff090a4ac3469358e6e418a42f710495b
EvilNeighbor <denissycheuski@gmail.com>
2022-04-11 18:15:35 +0300

    Условия и цыклы

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@
 
 //  First home work
 
-let numberOfFilms = +prompt("Сколько фильмов вы посмотрели", "");
+let numberOfFilms = prompt("Сколько фильмов вы посмотрели", "");
 let personalMovieDB = {
   count: numberOfFilms,
   movies: {},
@@ -54,16 +54,116 @@ let personalMovieDB = {
   privat: false,
 };
 
-const a = prompt("Один из последних просмотренных фильмов?"),
-  b = prompt("На сколько оцените его?"),
-  c = prompt("Один из последних просмотренных фильмов?"),
-  d = prompt("На сколько оцените его?");
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = prompt("На сколько оцените его?", "");
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("Allright");
+  } else {
+    console.log("So bad");
+    i--;
+  }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (personalMovieDB.count < 10) {
+  console.log("So few films watched");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log("You are classic  viewer");
+} else if (personalMovieDB.count >= 30) {
+  console.log("You are best viewer");
+} else {
+  console.log("Error");
+}
 
 console.log(personalMovieDB);
 
-//  First home work
-//  Second home work
+// let i = 0;
+// while (i < 2) {
+//   const a = prompt("Один из последних просмотренных фильмов?", ""),
+//     b = prompt("На сколько оцените его?", "");
+//   i++;
+//   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//     console.log("Allright");
+//   } else {
+//     console.log("so bad");
+//     i--;
+//   }
+// }
+
+// do {
+//   const a = prompt("Один из последних просмотренных фильмов?", ""),
+//     b = prompt("На сколько оцените его?", "");
+//   i++;
+//   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//     console.log("Allright");
+//   } else {
+//     console.log("so bad");
+//     i--;
+//   }
+// } while (i < 2);
+
+// Циклы
+// let num = 50;
+// // while (num <= 55) {
+// //   console.log(num);
+// //   ++num;
+// // }
+// let i = 0;
+
+//  First home work утв
+// Условия
+// if (4 == 4) {
+//   console.log("OK");
+// } else {
+//   console.log("rerro");
+// }
+// const nub = 99.9;
+// if (nub > 200) {
+//   console.log("Nub > 200");
+// } else if (nub < 100) {
+//   console.log("Nub < 100");
+// } else {
+//   console.log("OK");
+// }
+
+// const num = "51";
+// switch (num) {
+//   case "49":
+//     console.log("Wrong");
+//     break;
+//   case "100":
+//     console.log("Wrong2");
+//     break;
+//   case "50":
+//     console.log("Good");
+//     break;
+//   default:
+//     console.log("Not now");
+//     break;
+// }
+
+// Циклы
+// let num = 50;
+// // while (num <= 55) {
+// //   console.log(num);
+// //   ++num;
+// // }
+
+// // do {
+// //   console.log(num);
+// //   num++;
+// // } while (num < 55);
+
+// for (let i = 1; i < 10; ++i) {
+//   if (i === 7) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+//  Second homme work
+
 //  Second home work
